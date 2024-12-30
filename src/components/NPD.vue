13ba2a63fb3d032534402250077f8e1208f7b36f
Alan Ostaszynski <alan.ostaszynski@simplifyvms.com>
2024-12-30 12:21:31 -0300

    change display on chips and newItem

--- src/components/NPD.vue
+++ src/components/NPD.vue
@@ -29,15 +29,19 @@ const comensalRule = [
             <h6 class="text-h6 font-weight-bold">
               Comensales ({{ mesa.comensales.length }})
             </h6>
-            <v-chip
-              v-for="comensal in mesa.comensales"
-              :key="comensal.id"
-              class="ma-1"
-              closable
-              @click:close="mesa.removeComensal(comensal.id)"
-            >
-              {{ comensal.nombre }}
-            </v-chip>
+            <v-container fluid px-0>
+              <v-row>
+                <v-chip
+                  v-for="comensal in mesa.comensales"
+                  :key="comensal.id"
+                  class="ma-1"
+                  closable
+                  @click:close="mesa.removeComensal(comensal.id)"
+                >
+                  {{ comensal.nombre }}
+                </v-chip>
+              </v-row>
+            </v-container>
             <v-text-field
               v-model="mesa.newComensal"
               class="width-50"
@@ -70,47 +74,22 @@ const comensalRule = [
               prefix="$"
             />
 
-            <v-dialog width="auto" scrollable>
-              <template v-slot:activator="{ props: activatorProps }">
-                <v-btn
-                  block
-                  color="grey"
-                  text="Paga..."
-                  variant="outlined"
-                  v-bind="activatorProps"
+            <v-container fluid px-0>
+              <v-row>
+                <span>Paga...</span>
+              </v-row>
+              <v-row>
+                <v-checkbox
+                  v-for="comensal in mesa.comensales"
+                  :key="comensal.id"
+                  v-model="mesa.newItem.paga"
+                  class="pa-0 ma-0 ga-0"
+                  hide-details="true"
+                  :label="comensal.nombre"
+                  :value="comensal.id"
                 />
-              </template>
-
-              <template v-slot:default="{ isActive }">
-                <!-- <v-card prepend-icon="mdi-earth" title="Select Country"> -->
-                <v-card>
-                  <!-- <v-divider class="mt-3" /> -->
-                  <v-card-text class="px-4" style="height: 300px">
-                    <v-checkbox
-                      v-for="comensal in mesa.comensales"
-                      :key="comensal.id"
-                      v-model="mesa.newItem.paga"
-                      class="pa-0 ma-0 ga-0"
-                      hide-details="true"
-                      :label="comensal.nombre"
-                      :value="comensal.id"
-                    />
-                  </v-card-text>
-                  <v-divider />
-                  <v-card-actions>
-                    <v-btn text="Cerrar" @click="isActive.value = false" />
-                    <v-spacer />
-                    <v-btn
-                      color="surface-variant"
-                      text="Guardar"
-                      variant="flat"
-                      @click="isActive.value = false"
-                    />
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-            <!-- </div> -->
+              </v-row>
+            </v-container>
 
             <v-btn block class="my-2" color="primary" @click="mesa.addItem()"> + </v-btn>
 
